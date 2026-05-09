da05e990e5a5020aa52c4696bd86f220a0adf300
Paulenas11 <pauliuspuidokas10@gmail.com>
2026-05-09 15:45:42 +0300

    Fixed process crashes

--- src/app.py
+++ src/app.py
@@ -49,6 +49,7 @@ def udp_listener_thread():
     global rssi_data, esp_last_seen
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((UDP_IP, UDP_PORT))
 
     print("=== UDP LISTENER STARTED ===")
